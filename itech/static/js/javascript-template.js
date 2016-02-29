8ca7eff013c4d6d211c9ac7ee3373fa6515638c6
rorybain <2026138b@student.gla.ac.uk>
2016-02-29 12:35:26 +0000

    Updated templates with post pun functionality

---
+++ itech/static/js/javascript-template.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+    //$('#loginForm').formValidation({
+    //    framework: 'bootstrap',
+    //    icon: {
+    //        valid: 'glyphicon glyphicon-ok',
+    //        invalid: 'glyphicon glyphicon-remove',
+    //        validating: 'glyphicon glyphicon-refresh'
+    //    },
+    //    fields: {
+    //        username: {
+    //            validators: {
+    //                notEmpty: {
+    //                    message: 'The username is required'
+    //                }
+    //            }
+    //        },
+    //        password: {
+    //            validators: {
+    //                notEmpty: {
+    //                    message: 'The password is required'
+    //                }
+    //            }
+    //        }
+    //    }
+    //});
+
+    // Login button click handler
+    $('#loginButton').on('click', function () {
+        bootbox
+            .dialog({
+                title: 'New Pun',
+                message: $('#loginForm'),
+                show: false // We will show it manually later
+            })
+            .on('shown.bs.modal', function () {
+                $('#loginForm')
+                    .show()                             // Show the login form
+                    .formValidation('resetForm', true); // Reset form
+            })
+            .on('hide.bs.modal', function (e) {
+                // Bootbox will remove the modal (including the body which contains the login form)
+                // after hiding the modal
+                // Therefor, we need to backup the form
+                $('#loginForm').hide().appendTo('body');
+            })
+            .modal('show');
+    });
+
+    $('#tokenfield').tokenfield({
+        //autocomplete: {
+        //    source: ['red', 'blue', 'green', 'yellow', 'violet', 'brown', 'purple', 'black', 'white'],
+        //    delay: 100
+        //},
+        showAutocompleteOnFocus: true
+    });
+});
