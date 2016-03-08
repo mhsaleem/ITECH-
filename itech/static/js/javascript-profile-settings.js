c8e2920940310c147c2f1409ec9083777932f080
rorybain <2026138b@student.gla.ac.uk>
2016-03-08 11:38:05 +0000

    Added badge picker in profile

---
+++ itech/static/js/javascript-profile-settings.js
@@ -0,0 +1,12 @@
+/**
+ * Created by rorybain on 08/03/2016.
+ */
+jQuery(document).ready(function($) {
+
+    /**
+     * Example 1
+     * Load from SELECT field
+     */
+    $('#e1_element').fontIconPicker();
+
+});
