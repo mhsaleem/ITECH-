bc73efc4a82ece2a38c524fb6fe4bc8d7e8062ae
rorybain <2026138b@student.gla.ac.uk>
2016-03-11 16:13:27 +0000

    Changed a lot of the population script and updated db. Make sure to run each again

--- itech/populate_punny.py
+++ itech/populate_punny.py
@@ -4,13 +4,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'itech.settings')
 import django
 django.setup()
 
-from django.contrib.auth.models import User
 from punny.models import UserProfile, Title, Badge, Pun, Tag
+from django.contrib.auth.models import User
 
 
 def populate():
 
-    user_r = add_user(name='Rory')
+    user_r = add_user(name='rory', password='rory')
 
     user_profile = add_user_profile(user_r)
 
@@ -30,23 +30,23 @@ def populate():
 
     pun = add_pun(
         text="This is a pun",
-        owner=user_profile,
+        owner=user_r,
         tags=[tag]
     )
 
     pun = add_pun(
         text="Whiteboards are remarkable!",
-        owner=user_profile,
+        owner=user_r,
         tags=[tag]
     )
 
     pun = add_pun(
         text="Leif me alone....",
-        owner=user_profile,
+        owner=user_r,
         tags=[tag]
     )
 
-    user_h = add_user('Hashim')
+    user_h = add_user('hashim', 'hashim')
 
     user_profile = add_user_profile(user_h)
 
@@ -65,7 +65,7 @@ def populate():
 
     pun = add_pun(
         text="This is another pun",
-        owner=user_profile,
+        owner=user_h,
         tags=[tag, otherTag]
     )
 
@@ -75,19 +75,22 @@ def populate():
     for t in Title.objects.filter(user=user_r):
         print "- {0} - {1}".format(str(user_r), str(t))
     for p in Pun.objects.filter(owner=user_r):
-        print "- {0} - {1} = {2}".format(str(user_r), str(p), str(p.tags))
+        print "- {0} - {1}".format(str(user_r), str(p))
+        for tag in Tag.objects.filter(pun__tags__pun=p):
+            print "- {0}".format('#'+str(tag.text)+", ")
         # for t in Tag.objects.filter(pun=p):
         #     print "-- {0} - {1}".format(str(p), str(t))
 
 
-def add_user(name):
-    u = User.objects.get_or_create(username=name)[0]
+def add_user(name, password):
+    u = User.objects.get_or_create(username=name, password=password)[0]
     u.save()
     return u
 
 
 def add_user_profile(user):
     u = UserProfile.objects.get_or_create(user=user)[0]
+    u.picture = '/static/images/leonardo-dicaprio.jpg'
     u.save()
     return u
 
