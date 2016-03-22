5dfe96d92bac3bbc878cc546df327c7d80f2a8d6
mohammedhashimsaleem <2018657s@student.gla.ac.uk>
2016-03-22 14:33:33 +0000

    Merge branch 'master' of /Users/mohammedhashimsaleem/Desktop/ITECH/ITECH- with conflicts.

--- itech/punny/views.py
+++ itech/punny/views.py
@@ -256,3 +256,9 @@ def settings(request):
         response.set_cookie('virgin', 'this is a first time user', max_age=4)
 
     return response
+
+def handler404(request):
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
